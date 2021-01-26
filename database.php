64eaadfc36bdcb64cbb2cc1d453bb8adb611b810
Mees Gelein <meesgelein@gmail.com>
2021-01-26 12:43:51 +0100

    Working on database security

--- database.php
+++ database.php
@@ -1,7 +1,23 @@
 <?php
+require_once('./data/credentials.php');
+
 class DB extends SQLite3 {
 
     function __construct($url){
         $this->open($url);
+    }
+}
+
+class CredentialDB extends DB{
+
+    function __construct(){
+        parent::__construct('./data/credentials.db');
+    }
+}
+
+class CorpusDB extends DB{
+
+    function __construct(){
+        parent::__construct('./data/corpus.db');
     }
 }
--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 require_once('./database.php');
 
-$db = new DB('./data/test.db');
-if(!$db) {
-    echo $db->lastErrorMsg();
-} else {
-    echo "Opened database successfully\n";
-}
+$credentials = new CredentialDB();
+$corpus = new CorpusDB();
