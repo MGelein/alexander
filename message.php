f4dc971334d58ad854bb4c8eb1b147109e8b990d
Mees Gelein <meesgelein@gmail.com>
2021-01-27 01:56:11 +0100

    Forgot one endppoint

--- message.php
+++ message.php
@@ -2,6 +2,7 @@
 require_once('./const.php');
 require_once('./database.php');
 session_start();
+if(!is_logged_in()) exit();
 header('Content-Type: application/json');
 
 $json = json_decode(file_get_contents('php://input'), true);
