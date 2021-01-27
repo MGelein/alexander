ef36aa905d670e47f3b0cd9af319973dbf478fcd
Mees Gelein <meesgelein@gmail.com>
2021-01-27 01:53:44 +0100

    Only allow access to the api endpoints once a session has been established

--- text.php
+++ text.php
@@ -2,10 +2,10 @@
 require_once('./const.php');
 require_once('./database.php');
 session_start();
+if(!is_logged_in()) exit();
 header('Content-Type: application/json');
 
 $json = json_decode(file_get_contents('php://input'), true);
 if(!$json || !isset($json['action'])) exit();
 
 $action = strtolower($json['action']);
-
