b576b538a1030377976e52b069b743d8d1f3f97b
Mees Gelein <meesgelein@gmail.com>
2021-01-27 01:51:53 +0100

    Added boilerplate code for all api endpoints

--- message.php
+++ message.php
@@ -1,1 +1,10 @@
 <?php
+require_once('./const.php');
+require_once('./database.php');
+session_start();
+header('Content-Type: application/json');
+
+$json = json_decode(file_get_contents('php://input'), true);
+if(!$json || !isset($json['action'])) exit();
+
+$action = strtolower($json['action']);
